066856169a48070f61d0fd018b86fe0d3db7f0ea
NafisaTabassum01 <tabassumnafisa28.01@gmail.com>
2026-04-17 01:48:51 +0600

    btn background

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,26 +1,52 @@
 import React from 'react';
 import navlogo from "../../assets/logo.png";
-import { Link } from 'react-router';
-
-
-
+import { NavLink } from 'react-router';
 
 const Navbar = () => {
     return (
-        <div className=' shadow shadow-gray-200 pb-2'>
+        <div className='shadow shadow-gray-200 pb-2'>
             <div className='w-11/12 mx-auto md:flex justify-between'>
-            <div className='flex justify-center md:justify-start mt-2'>
-                <img src={navlogo} alt="" />
+
+                <div className='flex justify-center md:justify-start mt-2'>
+                    <img src={navlogo} alt="" />
                 </div>
-            <div className='flex justify-center gap-2 md:justify-end mt-2'>
-                <Link to="/"><button className='btn bg-[#244D3F] text-white'><i className="fa-regular fa-house"></i>Home</button></Link>
-                <Link to="/timeLine" className='btn border-none bg-white shadow-none'><i className="fa-regular fa-clock"></i>Timeline</Link>
-                <Link to="/charts" className='btn border-none bg-white shadow-none'><i className="fa-solid fa-chart-line"></i>Stats</Link>
+
+                <div className='flex justify-center gap-2 md:justify-end mt-2'>
+
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive
+                                ? "btn bg-[#244D3F] text-white"
+                                : "btn bg-white shadow-none"}>
+
+                        <i className="fa-regular fa-house"></i> Home
+                    </NavLink>
+
+                    <NavLink
+                        to="/timeLine"
+                        className={({ isActive }) =>
+                            isActive
+                                ? "btn bg-[#244D3F] text-white"
+                                : "btn bg-white shadow-none"}>
+
+                        <i className="fa-regular fa-clock"></i> Timeline
+                    </NavLink>
+
+                    <NavLink
+                        to="/charts"
+                        className={({ isActive }) =>
+                            isActive
+                                ? "btn bg-[#244D3F] text-white"
+                                : "btn bg-white shadow-none"}>
+                        <i className="fa-solid fa-chart-line"></i> Stats
+                    </NavLink>
+
+                </div>
+
             </div>
-        </div>
         </div>
     );
 };
-
 
 export default Navbar;
